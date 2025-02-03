6eda4aba9e2ee965a25e1f6b7f68e75c9616ad24
kosteranu <101293397+kosteranu@users.noreply.github.com>
2025-02-03 23:46:50 +0300

    Лаб 3

---
+++ Лаб 3/Лаб 3/task_1.py
@@ -0,0 +1,75 @@
+class Book:
+    """ Базовый класс книги. """
+    def __init__(self, name: str, author: str):
+        self._name = name
+        self._author = author
+
+    @property
+    def name(self):
+        """Доступ только для чтения"""
+        return self._name
+
+    @property
+    def author(self):
+        """Доступ только для чтения"""
+        return self._author
+
+    def __str__(self):
+        return f"Книга {self._name}. Автор {self._author}"
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r})"
+
+
+class PaperBook(Book):
+    """ Наследованный класс бумажной книги от класса книги. """
+    def __init__(self, name: str, author: str, pages: int):
+        super().__init__(name, author)
+        self.pages = pages
+
+    @property
+    def pages(self) -> int:
+        """Возвращает количество страниц в книге."""
+        return self._pages
+
+    @pages.setter
+    def pages(self, new_pages: int) -> None:
+        """Устанавливает количество страниц в книге."""
+        if not isinstance(new_pages, int):
+            raise TypeError("Количество страниц должно быть типа int")
+        if new_pages <= 0:
+            raise ValueError("Количество страниц должно быть положительным числом")
+        self._pages = new_pages
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r}, pages={self.pages!r})"
+
+
+class AudioBook(Book):
+    """ Наследованный класс аудиокниги от класса книги. """
+    def __init__(self, name: str, author: str, duration: float):
+        super().__init__(name, author)
+        self.duration = duration
+
+    @property
+    def duration(self) -> float:
+        """Возвращает продолжительность аудиокниги."""
+        return self._duration
+
+    @duration.setter
+    def duration(self, new_duration: float) -> None:
+        """Устанавливает продолжительность аудиокниги."""
+        if not isinstance(new_duration, float):
+            raise TypeError("Продолжительность должна быть типа float")
+        if new_duration <= 0:
+            raise ValueError("Продолжительность должна быть положительным числом")
+        self._duration = new_duration
+
+    def __repr__(self):
+        return f"{self.__class__.__name__}(name={self._name!r}, author={self._author!r}, pages={self.duration!r})"
+
+
+paper = PaperBook("1984", "Orwell", 200)
+print(repr(paper))
+audio = AudioBook("1984", "Orwell", 30.2)
+print(repr(audio))
